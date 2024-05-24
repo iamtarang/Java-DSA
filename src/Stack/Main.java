6f614f0b8c9fba63f03b4f400baae8ee84956bc9
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2024-05-24 13:42:32 +0530

    Custom Stack implemetation

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -1,5 +1,19 @@
 package Stack;
 
 public class Main {
-	
+	public static void main(String[] args) throws StackException {
+		CustomStack stack = new CustomStack();
+
+		stack.push(34);
+		stack.push(22);
+		stack.push(94);
+		stack.push(67);
+		stack.push(-26);
+
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
 }
